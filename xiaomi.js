49a100487244a75a14272d2188b8fa28a01bb7a3
lzy22 <jdblzy@icloud.com>
2018-03-10 08:41:20 +0800

    1

--- xiaomi.js
+++ xiaomi.js
@@ -132,44 +132,110 @@
 
 }
 
-//内容效果
-{
-	const prev=document.querySelector(".neirong_item .nr_prev");
-	const next=document.querySelector(".neirong_item .nr_next");
-	const inner=document.querySelector(".nr_bottom_inner");
-	let n=0;    //代表当前往左边移动几屏
-	next.onclick=function(){
-		n++;
-		inner.style.marginLeft=-310*n+"px";
-	}
-	prev.onclick=function(){
-		n--;
-		inner.style.marginLeft=-310*n+"px";
-	}
 
+//banner侧导航效果
+{
+	const labels=document.querySelectorAll(".banner_nav li");
+	let menus=document.querySelectorAll(".banner_nav_menu");
+	let obj=menus[0];
+	labels.forEach(function(ele,index){
+		ele.onmouseenter=function(){
+			obj.style.display="none";
+			menus[index].style.display="block";
+			// obj=menus[index];
+		}
+		ele.onmouseleave=function(){
+			menus[index].style.display="none";
+		}
+	})
 }
 
-//智能效果
+//导航效果
 {
-	const contentList=document.querySelectorAll(".zhineng");
+	const tops=document.querySelectorAll(".banner_top li");
+	let bottoms=document.querySelector(".banner_bottom");
+	tops.forEach(function(ele,index){
+		ele.onmouseenter=function(){
+			bottoms.style.height="239px";
+		}
+		ele.onmouseleave=function(){
+			bottoms.style.height="0";
+		}
+	})
+		
+}
+
+//内容效果
+{
+	function content(parents){
+		let prevs=parents.querySelector(".nr_prev");
+		let nexts=parents.querySelector(".nr_next");
+		let inners=parents.querySelector(".nr_bottom_inner");
+		let content=parents.querySelectorAll(".neirong_item");
+		let pagers=parents.querySelectorAll(".xpage");
+		let n=0;    //代表当前往左边移动几屏
+		nexts.onclick=function(ele,index){
+			n++;
+			if(n===content.length){
+				n=content.length-1;
+				return;
+			}
+			inners.style.marginLeft=n*-310+"px";
+			pagers[n].classList.add("active00");
+			pagers[n-1].classList.remove("active00");
+		}
+		prevs.onclick=function(ele,index){
+			n--;
+			if(n<0){
+				n=0;
+				return;
+			}
+			let obj=pagers[0];
+			inners.style.marginLeft=n*-310+"px";
+			pagers[n].classList.add("active00");
+			pagers[n+1].classList.remove("active00");
+
+			pagers.forEach(function(ele,index){
+				ele.onclick=function(){
+					obj.classList.remove("active00");
+					ele.classList.add("active00");
+					obj=this;
+					inners.style.marginLeft=-310*n+"px";
+					n=index;
+				}
+			})
+
+		}
+	}
+	const contentList=document.querySelectorAll(".neirong_bottom01");
 	contentList.forEach(function(ele){
 		content(ele);
 	});
+
+}
+
+
+//智能效果
+{
+	
 	function content(parent){
 		var btns=parent.querySelectorAll(".zhineng_more span");
 		var cons=parent.querySelectorAll(".zhineng .zhineng_bottom");
 		for(let i=0;i<cons.length;i++){
 			btns[i].onmouseenter=function(){
 			for(let j=0;j<cons.length;j++){
-			btns[j].classList.remove("active3");
+			// btns[j].classList.remove("active3");
 			cons[j].style.zIndex="";
 		}
-		cons[i].style.zIndex=10;
-		
+		cons[i].style.zIndex=10;	
 	}
 	
 	}
 	
 	}
+	const contentList=document.querySelectorAll(".zhineng");
+	contentList.forEach(function(ele){
+		content(ele);
+	});
 
 }
